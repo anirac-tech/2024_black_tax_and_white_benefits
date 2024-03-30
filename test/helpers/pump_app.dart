cbe6ea30a0c7057a96aadb05dc76d1c0e98a6191
neiljaywarner <njwandroid@gmail.com>
2024-03-30 14:50:40 -0500

    fix analysis issues for pipeline.

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,15 +1,10 @@
-import 'package:black_tax_and_white_benefits/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(Widget widget) {
-    return pumpWidget(
-      MaterialApp(
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        home: widget,
-      ),
-    );
-  }
+  Future<void> pumpApp(Widget widget) => pumpWidget(
+        MaterialApp(
+          home: widget,
+        ),
+      );
 }
