ab63192fc141e4fc5b4b3d94b75075c60fd90fa4
neiljaywarner <njwandroid@gmail.com>
2024-03-30 15:11:55 -0500

    remove commented out code

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -5,17 +5,13 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
-      ),
-      //localizationsDelegates: AppLocalizations.localizationsDelegates,
-      //supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
-    );
-  }
+        home: const CounterPage(),
+      );
 }
