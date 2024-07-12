e5cee15e1996990e80fa0d98422e5cbf1d197ff5
alexdivadi <dawork20100@gmail.com>
2024-07-12 16:40:43 -0700

    Add staging firebase app

--- lib/env/initialize_firebase.dart
+++ lib/env/initialize_firebase.dart
@@ -1,13 +1,14 @@
 import 'package:black_tax_and_white_benefits/env/flavor.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:black_tax_and_white_benefits/firebase_options.dart' as prod;
+import 'package:black_tax_and_white_benefits/firebase_options_stg.dart' as stg;
 import 'package:black_tax_and_white_benefits/firebase_options_dev.dart' as dev;
 
 Future<void> initializeFirebaseApp() async {
   final flavor = getFlavor();
   final firebaseOptions = switch (flavor) {
     Flavor.prod => prod.DefaultFirebaseOptions.currentPlatform,
-    Flavor.stg => dev.DefaultFirebaseOptions.currentPlatform,
+    Flavor.stg => stg.DefaultFirebaseOptions.currentPlatform,
     Flavor.dev => dev.DefaultFirebaseOptions.currentPlatform,
   };
   await Firebase.initializeApp(
